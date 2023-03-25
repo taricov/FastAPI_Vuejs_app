3cd266c11c08935c0ba82d251511d9072d8e09d8
TaricOv <tareq.ouf@gmail.com>
2023-03-25 15:59:17 +0200

    added dotenv variables to the server

--- server/main.py
+++ server/main.py
@@ -4,15 +4,19 @@ from tortoise import fields
 from tortoise.contrib.fastapi import register_tortoise
 from tortoise.contrib.pydantic import pydantic_model_creator
 from tortoise.models import Model
-
+from dotenv import load_dotenv
+import os
 
 app = FastAPI()
+load_dotenv()
 
 
 
-# @app.get("/")
-# async def fetch_all():
-# 	return {"name": "Taricov"}
+
+
+@app.get("/")
+async def fetch_all():
+	return {"name": os.getenv("JWT_TOKEN")}
 
 # @app.get("/items/{item_id}")
 # async def read_item(item_id: int, q: Union[str, None] = None):
