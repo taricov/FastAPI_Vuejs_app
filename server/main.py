8bf39108121cc4337b3f6eca71e125e561d6e90d
TaricOv <tareq.ouf@gmail.com>
2023-03-25 15:51:44 +0200

    added all needed libs for my fasyapi server

--- server/main.py
+++ server/main.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI
-from typing import Union
-
+# from typing import Union
+from tortoise import fields
+from tortoise.contrib.fastapi import register_tortoise
+from tortoise.contrib.pydantic import pydantic_model_creator
+from tortoise.models import Model
 
 
 app = FastAPI()
 
-@app.get("/")
-async def fetch_all():
-	return {"name": "Taricov"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+
+# @app.get("/")
+# async def fetch_all():
+# 	return {"name": "Taricov"}
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
