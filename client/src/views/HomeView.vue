7de9ff45fa34276fcceb1b43b819792f11f5bbee
TaricOv <tareq.ouf@gmail.com>
2023-03-26 23:16:06 +0200

    fixed prettier warnings and ready to rock on

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <div class="bg-slate-300">hi, there</div>
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <hello-world />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+// Components
+import HelloWorld from "../components/HelloWorld.vue";
 
 export default defineComponent({
   name: "HomeView",
+
   components: {
     HelloWorld,
   },
