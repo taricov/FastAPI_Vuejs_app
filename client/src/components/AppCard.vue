51b8bafd52f3f62f9e531ea279243d6bbf7ad0aa
TaricOv <tareq.ouf@gmail.com>
2023-03-29 22:03:04 +0200

    finished card design

--- client/src/components/AppCard.vue
+++ client/src/components/AppCard.vue
@@ -15,23 +15,29 @@ export default defineComponent({
 <template>
   <v-card
     :loading="loading"
-    class="shadow-none mx-auto my-12 rounded-xl bg-gradient-to-b bg-transparent from-_darkest to-transparent"
+    class="text-white md:mx-3 shadow-none my-12 rounded-xl bg-gradient-to-b bg-transparent from-_darkest to-transparent"
     max-width="374"
   >
-    <v-img cover height="250" src="img"></v-img>
+    <v-img
+      cover
+      height="250"
+      src="https://cdna.artstation.com/p/assets/images/images/033/217/864/large/taric-ov-maskedboy.jpg?1608785841"
+    ></v-img>
 
-    <v-card-item>
-      <v-card-title>{{ title }}</v-card-title>
+    <v-card-item class="pb-2">
+      <v-card-title class="text-[18px] overflow-auto whitespace-normal">{{
+        title
+      }}</v-card-title>
 
-      <v-card-subtitle>
-        <span class="me-1 opacity-50">{{ owner }}</span>
-
-        <v-icon color="error" icon="mdi-fire-circle" size="small"></v-icon>
+      <v-card-subtitle class="mt-5">
+        <span class="opacity-30 uppercase font-semibold">{{ owner }}</span>
       </v-card-subtitle>
     </v-card-item>
 
     <v-card-text>
-      <div class="my-4 text-subtitle-1">{{ views }} views - {{ date }} ago</div>
+      <div class="text-xs text-slate-300">
+        {{ views }} views - {{ date }} ago
+      </div>
     </v-card-text>
   </v-card>
 </template>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,15 +1,25 @@
 <template>
   <nav-bar />
   <cats-tabs />
-  <div v-for="n in 6" :key="n">
-    <app-card
-      :title="cards[0].title"
-      :owner="cards[0].owner"
-      :date="cards[0].date"
-      :views="cards[0].views"
-      :img="cover(cards[0].imgUrl)"
-    />
-  </div>
+
+  <v-row no-gutters>
+    <v-col
+      v-for="n in 6"
+      :key="n"
+      cols="12"
+      sm="4"
+      class="flex items-center justify-center"
+    >
+      <app-card
+        :title="cards[0].title"
+        :owner="cards[0].owner"
+        :date="cards[0].date"
+        :views="cards[0].views"
+        :img="cards[0].imgUrl"
+      />
+    </v-col>
+  </v-row>
+
   <app-layout> </app-layout>
 </template>
 
@@ -25,14 +35,7 @@ import AppCard from "../components/AppCard.vue";
 export default defineComponent({
   name: "HomeView",
   methods: {
-    cover(url: string) {
-      if (url !== "") {
-        try {
-          url = require(url);
-        } catch (e) {
-          url = "fef";
-        }
-      } else url = "dd";
+    loadImg(url: string) {
       return url;
     },
   },
@@ -44,6 +47,7 @@ export default defineComponent({
   },
   data() {
     return {
+      imgurlval: "",
       cards: [
         {
           title: "Design and Prototype an App with Play - Full 3-hour Course",
@@ -51,7 +55,7 @@ export default defineComponent({
           views: "14k",
           date: "1 month",
           imgUrl:
-            "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2dacb81b-fca7-4c16-a479-a904161aff2c/dft0gkv-bdb679fb-63ce-4eb8-b71a-65c9da43f664.jpg/v1/fill/w_1095,h_730,q_70,strp/on_the_bones_of_men_by_evrthedrkai_dft0gkv-pre.jpg",
+            "https://cdna.artstation.com/p/assets/images/images/033/217/864/large/taric-ov-maskedboy.jpg?1608785841",
         },
       ],
     };
